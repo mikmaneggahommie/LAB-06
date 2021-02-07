702ea8dbc5ec6516b406b1ab60a2a7abc638d855
mikiyas daniel <it.mikiyas.daniel@gmail.com>
2021-02-07 20:18:32 +0300

    approrite path and edit html

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,215 +1,229 @@
-// Define UI Variables 
 const taskInput = document.querySelector('#task'); //the task input text field
+
 const form = document.querySelector('#task-form'); //The form at the top
+
 const filter = document.querySelector('#filter'); //the task filter text field
-const taskList = document.querySelector('.collection'); //The UL
+
+const taskList = document.querySelector('.collection'); //The ul
+
 const clearBtn = document.querySelector('.clear-tasks'); //the all task clear button
 
-const reloadIcon = document.querySelector('.fa'); //the reload button at the top navigation 
+const reloadBtn = document.querySelector('.fa'); // the reload button
 
 const query = document.querySelector('.collection'); //      I've used querySelector for the .collection
 
 const allLi = query.getElementsByTagName('li'); //I've used by TagName method to identify every collection-item
 
-//DB variable 
 
+//Declare DB var
 let DB;
-
-
-
 // Add Event Listener [on Load]
 document.addEventListener('DOMContentLoaded', () => {
-    // create the database
-    let TasksDB = indexedDB.open('tasks', 1);
-        
-    // if there's an error
-    TasksDB.onerror = function() {
-            console.log('There was an error');
-        }
-        // if everything is fine, assign the result to the instance
-    TasksDB.onsuccess = function() {
-        // console.log('Database Ready');
-
-        // save the result
-        DB = TasksDB.result;
-
-        // display the Task List 
-        displayTaskList();
+  function displayTaskList() {
+    filter.addEventListener('keyup', filterTasks);
+    // clear the previous task list
+    while (taskList.firstChild) {
+      taskList.removeChild(taskList.firstChild);
     }
 
-    // This method runs once (great for creating the schema)
-    TasksDB.onupgradeneeded = function(e) {
-        // the event will be the database
-        let db = e.target.result;
+    // create the object store
+    let objectStore = DB.transaction('tasks').objectStore('tasks');
 
-        // create an object store, 
-        // keypath is going to be the Indexes
-        let objectStore = db.createObjectStore('tasks', { keyPath: 'id', autoIncrement: true });
+    objectStore.openCursor().onsuccess = function (e) {
+      // assign the current cursor
+      let cursor = e.target.result;
 
-        // createindex: 1) field name 2) keypath 3) options
-        objectStore.createIndex('taskname', 'taskname', { unique: false });
+      if (cursor) {
+        const li = document.createElement('li');
+        li.className = 'collection-item';
+        li.appendChild(document.createTextNode(taskInput.value));
+        //Create new element for the link
+        const dateID = Date.now();
+        const link = document.createElement('a');
+        link.className = "delete-item secondary-content";
+        // link.innerHTML = '<i class="fa fa-remove"></i>';
+        link.innerHTML = `<i class="fa fa-remove"></i>  &nbsp; <a href="edit.html?id=${cursor.value.id}"><i class="fa fa-edit"></i> </a>`;
+        const dateDiv = document.createElement("div");
+        dateDiv.className = "dateDiv";
+        dateDiv.style.display = "none";
+        dateDiv.textContent = dateID;
 
-        console.log('Database ready and fields created!');
+        li.appendChild(dateDiv);
+        li.setAttribute('data-task-id', cursor.value.id);
+        li.appendChild(link);
+        taskList.appendChild(li);
+        // Create text node and append it 
+        li.appendChild(document.createTextNode(cursor.value.taskname));
+        taskInput.value = '';
+        cursor.continue();
+
+      }
+    }
+  }
+
+  //all code will reside here 
+  let TasksDB = indexedDB.open("tasks", 1);
+  TasksDB.onsuccess = function () {
+    DB = TasksDB.result;
+    console.log('Database created.');
+    console.log(DB);
+    displayTaskList();
+  }
+  TasksDB.onerror = function () {
+    console.log('Some Error has happned.');
+  }
+  TasksDB.onupgradeneeded = function (e) {
+    // the event will be the database
+    let db = e.target.result;
+
+    // create an object store, 
+    // keypath is going to be the Indexes
+    let objectStore = db.createObjectStore('tasks', {
+      keyPath: 'id',
+      autoIncrement: true
+    });
+
+    // createindex: 1) field name 2) keypath 3) options
+    objectStore.createIndex('taskname', 'taskname', {
+      unique: false
+    });
+
+    console.log('Database ready and fields created!');
+  }
+
+  form.addEventListener('submit', addNewTask);
+
+  function addNewTask(e) {
+    e.preventDefault();
+    if (taskInput.value === '') {
+      taskInput.style.borderColor = 'red';
+      return;
+    }
+    taskInput.style.borderColor = 'green';
+    //add to DB
+    // create a new object with the form info
+    let newTask = {
+      taskname: taskInput.value,
+      date: new Date()
+    }
+    // Insert the object into the database 
+    let transaction = DB.transaction(['tasks'], 'readwrite');
+    let objectStore = transaction.objectStore('tasks');
+
+    let request = objectStore.add(newTask);
+    // on success
+    request.onsuccess = () => {
+      form.reset();
+      displayTaskList();
+
+
+    }
+    transaction.oncomplete = () => {
+      console.log('New appointment added');
+      //displayTaskList();
+    }
+    transaction.onerror = () => {
+      console.log('There was an error, try again!');
     }
 
-    form.addEventListener('submit', addNewTask);
+  }
+  // Remove task event [event delegation]
+  taskList.addEventListener('click', removeTask);
 
-    function addNewTask(e) {
-        e.preventDefault();
+  function removeTask(e) {
+    Number(e.target.parentElement.parentElement.getAttribute('data-task-id'));
 
-        // Check empty entry
-        if (taskInput.value === '') {
-            taskInput.style.borderColor = "red";
+    if (e.target.parentElement.classList.contains('delete-item')) {
+      if (confirm('Are You Sure about that ?')) {
+        // get the task id
 
-            return;
-        }
 
-        // create a new object with the form info
-        let newTask = {
-            taskname: taskInput.value,
-        }
-
-        // Insert the object into the database 
+        let taskID = Number(e.target.parentElement.parentElement.getAttribute('data-task-id'));
         let transaction = DB.transaction(['tasks'], 'readwrite');
-        let objectStore = transaction.objectStore('tasks');
+        // use a transaction
+        let objectStore = DB.transaction('tasks', 'readwrite').objectStore('tasks');
+        objectStore.delete(taskID);
 
-        let request = objectStore.add(newTask);
-
-        // on success
-        request.onsuccess = () => {
-            form.reset();
-        }
         transaction.oncomplete = () => {
-            console.log('New appointment added');
-
-            displayTaskList();
-        }
-        transaction.onerror = () => {
-            console.log('There was an error, try again!');
+          e.target.parentElement.parentElement.remove();
         }
 
-    }
-
-    function displayTaskList() {
-        // clear the previous task list
-        while (taskList.firstChild) {
-            taskList.removeChild(taskList.firstChild);
-        }
-
-        // create the object store
-        let objectStore = DB.transaction('tasks').objectStore('tasks');
-
-        objectStore.openCursor().onsuccess = function(e) {
-            // assign the current cursor
-            let cursor = e.target.result;
-
-            if (cursor) {
-
-                // Create an li element when the user adds a task 
-                const li = document.createElement('li');
-                //add Attribute for delete 
-                li.setAttribute('data-task-id', cursor.value.id);
-                // Adding a class
-                li.className = 'collection-item';
-                // Create text node and append it 
-                li.appendChild(document.createTextNode(cursor.value.taskname));
-                // Create new element for the link 
-                const link = document.createElement('a');
-                // Add class and the x marker for a 
-                link.className = 'delete-item secondary-content';
-                link.innerHTML = `
-                 <i class="fa fa-remove"></i>
-                &nbsp;
-                <a href="/Lesson 04 [Lab 06]/Finished/edit.html?id=${cursor.value.id}"><i class="fa fa-edit"></i> </a>
-                `;
-                // Append link to li
-                li.appendChild(link);
-                // Append to UL 
-                taskList.appendChild(li);
-                cursor.continue();
-            }
-        }
-    }
-
-    // Remove task event [event delegation]
-    taskList.addEventListener('click', removeTask);
-
-    function removeTask(e) {
-
-        if (e.target.parentElement.classList.contains('delete-item')) {
-            if (confirm('Are You Sure about that ?')) {
-                // get the task id
-                let taskID = Number(e.target.parentElement.parentElement.getAttribute('data-task-id'));
-                // use a transaction
-                let transaction = DB.transaction(['tasks'], 'readwrite');
-                let objectStore = transaction.objectStore('tasks');
-                objectStore.delete(taskID);
-
-                transaction.oncomplete = () => {
-                    e.target.parentElement.parentElement.remove();
-                }
-
-            }
-
-        }
-
-    }
-    $(".dropdown-trigger").dropdown();
-    const ascendingBtn = document.querySelector(".ascending-btn");
-    const descendingBtn = document.querySelector(".descending-btn");
-    const collectionSorted = document.querySelector(".collection-sorted");
-    //ascending Sort function
-    function ascendSort() {
-      const unorderedList = document.querySelectorAll(".collection-item");
-      var orderingArray = new Array();
-      const currentTime = Date.now();
-      for (let i = 0; i < unorderedList.length; i++) {
-        listItem = unorderedList[i].querySelector(".dateDiv");
-        taskListTime = listItem.textContent;
-        let differenceTime = currentTime - taskListTime;
-        orderingArray[i] = [differenceTime, i];
-      }
-      orderingArray.sort();
-      for (let i = 0; i < unorderedList.length; i++) {
-        collectionSorted.appendChild(unorderedList[orderingArray[i][1]]);
-      }
-      for (let i = 0; i < unorderedList.length; i++) {
-        taskList.appendChild(unorderedList[orderingArray[i][1]]);
       }
     }
-    // descending sort function
-    function descendSort() {
-      const unorderedList = document.querySelectorAll(".collection-item");
-      var orderingArray = new Array();
-      const currentTime = Date.now();
-      for (let i = 0; i < unorderedList.length; i++) {
-        listItem = unorderedList[i].querySelector(".dateDiv");
-        taskListTime = listItem.textContent;
-        let differenceTime = currentTime - taskListTime;
-        orderingArray[i] = [differenceTime, i];
-      }
-      orderingArray.sort();
-      orderingArray.reverse();
-      for (let i = 0; i < unorderedList.length; i++) {
-        collectionSorted.appendChild(unorderedList[orderingArray[i][1]]);
-      }
-      for (let i = 0; i < unorderedList.length; i++) {
-        taskList.appendChild(unorderedList[orderingArray[i][1]]);
+  }
+  //making the filter function
+  function filterTasks() {
+    let key = document.getElementById('filter').value; //key now has the filtered value
+    for (let i = 0; i < allLi.length; i++) {
+      if ((new RegExp(key)).test(allLi[i].textContent)) {
+        allLi[i].style.display = "";
+      } else {
+        allLi[i].style.display = "none";
       }
     }
-  
-    //clear button event listener   
-    clearBtn.addEventListener('click', clearAllTasks);
+  }
+  clearBtn.addEventListener('click', clearAllTasks);
+  //clear tasks 
+  function clearAllTasks() {
+    if (confirm("Are you sure you want to clear all tasks?")) {
+      //Create the transaction and object store
+      let transaction = DB.transaction("tasks", "readwrite");
+      let tasks = transaction.objectStore("tasks");
 
-    //clear tasks 
-    function clearAllTasks() {
-        let transaction = DB.transaction("tasks", "readwrite");
-        let tasks = transaction.objectStore("tasks");
-        // clear the table.
-        tasks.clear();
-        displayTaskList();
-        console.log("Tasks Cleared !!!");
+      // clear the the table
+      tasks.clear();
+      //repaint the UI
+      displayTaskList();
+
+      console.log("Tasks Cleared !!!");
     }
+  }
+  $(".dropdown-trigger").dropdown();
+  const ascendingBtn = document.querySelector(".ascending-btn");
+  const descendingBtn = document.querySelector(".descending-btn");
+  const collectionSorted = document.querySelector(".collection-sorted");
+  //ascending Sort function
+  function ascendSort() {
+    const unorderedList = document.querySelectorAll(".collection-item");
+    var orderingArray = new Array();
+    const currentTime = Date.now();
+    for (let i = 0; i < unorderedList.length; i++) {
+      listItem = unorderedList[i].querySelector(".dateDiv");
+      taskListTime = listItem.textContent;
+      let differenceTime = currentTime - taskListTime;
+      orderingArray[i] = [differenceTime, i];
+    }
+    orderingArray.sort();
+    for (let i = 0; i < unorderedList.length; i++) {
+      collectionSorted.appendChild(unorderedList[orderingArray[i][1]]);
+    }
+    for (let i = 0; i < unorderedList.length; i++) {
+      taskList.appendChild(unorderedList[orderingArray[i][1]]);
+    }
+  }
+  // descending sort function
+  function descendSort() {
+    const unorderedList = document.querySelectorAll(".collection-item");
+    var orderingArray = new Array();
+    const currentTime = Date.now();
+    for (let i = 0; i < unorderedList.length; i++) {
+      listItem = unorderedList[i].querySelector(".dateDiv");
+      taskListTime = listItem.textContent;
+      let differenceTime = currentTime - taskListTime;
+      orderingArray[i] = [differenceTime, i];
+    }
+    orderingArray.sort();
+    orderingArray.reverse();
+    for (let i = 0; i < unorderedList.length; i++) {
+      collectionSorted.appendChild(unorderedList[orderingArray[i][1]]);
+    }
+    for (let i = 0; i < unorderedList.length; i++) {
+      taskList.appendChild(unorderedList[orderingArray[i][1]]);
+    }
+  }
+
+  ascendingBtn.addEventListener("click", ascendSort);
+  descendingBtn.addEventListener("click", descendSort);
 
 
 });
